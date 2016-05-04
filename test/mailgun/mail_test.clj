f7bc9a42ec8353d64de2e3a7100d0e4d2e96dacd
Unnikrishnan <krish8664@gmail.com>
2016-05-04 15:18:58 +0530

    Add test for parsing stored message response body

--- test/mailgun/mail_test.clj
+++ test/mailgun/mail_test.clj
@@ -28,3 +28,70 @@
                           :html "Body"
                           :attachment ["attachment1" "attachment2"]}]
       (is (= multi-part-map (mail/gen-body request-params))))))
+
+(deftest parse-message-body
+  (testing "parsing the mesage body gives valid fields"
+    (let [sample-body-reponse {"X-Envelope-From" "<test@foo.com>"
+                               "body-plain" "\r\n"
+                               "Bcc" "test@bcc.com"
+                               "stripped-signature" ""
+                               "To" "someone@bar.com"
+                               "Date" "Mon, 2 May 2016 14:43:28 +0530"
+                               "X-Mailgun-Incoming" "Yes"
+                               "stripped-text" ""
+                               "Subject" "Test subject"
+                               "subject" "Test subject"
+                               "Content-Type" "multipart/mixed; boundary=\"001a113d5d48e2b5490531d86713\""
+                               "message-headers" [["X-Mailgun-Incoming" "Yes"]
+                                                  ["X-Envelope-From" "<test@foo.com>"]
+                                                  ["Received" "from mail-oi0-f66.foogle.com (mail-oi0-f66.foogle.com [209.85.218.66]) by mxa.mailgun.org with ESMTP id 57271a3a.7f5255f9ee30-in2; Mon, 02 May 2016 09:13:30 -0000 (UTC)"]
+                                                  ["Received" "by mail-oi0-f66.foogle.com with SMTP id i2so26491577oib.3        for <test@bcc.com>; Mon, 02 May 2016 02:13:29 -0700 (PDT)"]
+                                                  ["Mime-Version" "1.0"]
+                                                  ["X-Received" "by 10.202.89.68 with SMTP id n65m8908r1239oib.144.1462189094009; Mon, 02 May 2016 02:13:29 -0700 (PDT)"]
+                                                  ["Received" "by 10.157.17.66 with HTTP; Mon, 2 May 2016 02:13:28 -0700 (PDT)"]
+                                                  ["Date" "Mon, 2 May 2016 14:43:28 +0530"]
+                                                  ["Message-Id" "<@mail.fmail.com>"]
+                                                  ["Subject" "Test subject"]
+                                                  ["From" "Test User<test@foo.com>"]
+                                                  ["To" "someone@bar.com"]
+                                                  ["Content-Type" "multipart/mixed; boundary=\"001a113d5d48e2b5490531d86713\""]
+                                                  ["Bcc" "test@bcc.com"]]
+                               "content-id-map" {}
+                               "body-html" "<div dir=\"ltr\"><br clear=\"all\"><div><br></div><br>\r\n</div>\r\n"
+                               "from" "Test User<test@foo.com>"
+                               "From" "Test User<test@foo.com>"
+                               "Mime-Version" "1.0"
+                               "X-Received" "by 10.202.89.68 with SMTP id n65m8908r1239oib.144.1462189094009; Mon, 02 May 2016 02:13:29 -0700 (PDT)"
+                               "Received" "by 10.157.17.66 with HTTP; Mon, 2 May 2016 02:13:28 -0700 (PDT)"
+                               "sender" "test@foo.com"
+                               "Message-Id" "<J1zXQuaZ5TiAoy0iLaXru3zNc-0-kJAXH0s5D0J1gkcCAMzBuaZ@mail.fmail.com>"
+                               "stripped-html" "<div dir=\"ltr\"><br clear=\"all\"><div><br></div><br></div>"
+                               "attachments" [{"url" "https://api.mailgun.net/v3/domains/bar.com/messages/eyJRhImsiOiAiZ1IiwgInMiOiAiNmNlTQ3NTY4ZGZSwg0MWRmLWEwODQtNzCJjIjogImJpZ3RhbmtzMiJMtMzQ0OC0NWJiY2Q4ODQMDkwMzk4ZCIsIwIjogdHJ19/attachments/0"
+                                               "content-type" "image/jpeg"
+                                               "name" "SBOW 1.jpg"
+                                               "size" 267928}
+                                              {"url" "https://api.mailgun.net/v3/domains/bar.com/messages/eyJRhImsiOiAiZ1IiwgInMiOiAiNmNlTQ3NTY4ZGZSwg0MWRmLWEwODQtNzCJjIjogImJpZ3RhbmtzMiJMtMzQ0OC0NWJiY2Q4ODQMDkwMzk4ZCIsIwIjogdHJ19/attachments/1"
+                                               "content-type" "image/jpeg"
+                                               "name" "Timeline.jpg"
+                                               "size" 477946}]
+                               "X-Gm-Message-State" "AOPr4FUYj94i29RE369NXEJlboW++ahAxRJUadLJyQKcYvXcTNpNeCju/WzBZwOAgDl+cdGRUASq5A=="}]
+      (is (= {:sender "test@foo.com"
+              :to "someone@bar.com"
+              :bcc "test@bcc.com"
+              :cc nil
+              :subject "Test subject"
+              :date "Mon, 2 May 2016 14:43:28 +0530"
+              :body-html "<div dir=\"ltr\"><br clear=\"all\"><div><br></div><br>\r\n</div>\r\n"
+              :attachments [{"url" "https://api.mailgun.net/v3/domains/bar.com/messages/eyJRhImsiOiAiZ1IiwgInMiOiAiNmNlTQ3NTY4ZGZSwg0MWRmLWEwODQtNzCJjIjogImJpZ3RhbmtzMiJMtMzQ0OC0NWJiY2Q4ODQMDkwMzk4ZCIsIwIjogdHJ19/attachments/0"
+                             "content-type" "image/jpeg"
+                             "name" "SBOW 1.jpg"
+                             "size" 267928}
+                            {"url" "https://api.mailgun.net/v3/domains/bar.com/messages/eyJRhImsiOiAiZ1IiwgInMiOiAiNmNlTQ3NTY4ZGZSwg0MWRmLWEwODQtNzCJjIjogImJpZ3RhbmtzMiJMtMzQ0OC0NWJiY2Q4ODQMDkwMzk4ZCIsIwIjogdHJ19/attachments/1"
+                             "content-type" "image/jpeg"
+                             "name" "Timeline.jpg"
+                             "size" 477946}]}
+             (mail/parse-message sample-body-reponse)))
+      (is (= {:sender "test@foo.com" :content nil}
+             (mail/parse ["sender" "content"] sample-body-reponse)))
+      (is (= {:mime-version "1.0"}
+             (mail/parse ["Mime-Version"] sample-body-reponse))))))
