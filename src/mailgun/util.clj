02ccfdfba15058d9e8925ee58147dce044558df5
Unnikrishnan <krish8664@gmail.com>
2016-05-05 11:47:45 +0530

    Add util functions to-file and to-vector so as to improve how send-mail takes in the message params

--- src/mailgun/util.clj
+++ src/mailgun/util.clj
@@ -1,5 +1,6 @@
 (ns mailgun.util
-  (:require [cheshire.core :as json]))
+  (:require [cheshire.core :as json]
+            [clojure.java.io :as io]))
 
 (defn json-to-clj
   "Takes in a mailgun response and converts the json body to clojure map"
@@ -7,3 +8,18 @@
   (assoc-in response [:body] (-> response
                                  :body
                                  json/parse-string)))
+
+(defn to-vector
+  "Checks if `x` is a vector or not, if not then returns a vector `x`"
+  [x]
+  (if (sequential? x)
+    x
+    [x]))
+
+(defn to-file
+  "Converts a list of attachments to list of file objects"
+  [attachments]
+  (when-not (nil? attachments)
+    (->> attachments
+         to-vector
+         (map #(io/file %)))))
