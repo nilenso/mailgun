73d026fef5ac7a2cec83b4da02438348782fc787
Unnikrishnan <krish8664@gmail.com>
2016-05-12 22:52:52 +0530

    Code refactoring

--- src/mailgun/util.clj
+++ src/mailgun/util.clj
@@ -5,9 +5,7 @@
 (defn json-to-clj
   "Takes in a mailgun response and converts the json body to clojure map"
   [response]
-  (assoc-in response [:body] (-> response
-                                 :body
-                                 json/parse-string)))
+  (update response :body json/parse-string))
 
 (defn ensure-sequential
   "Checks if `x` is a vector or not, if not then returns a vector `x`"
